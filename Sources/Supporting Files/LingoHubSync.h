d4cad9e894325d4e93496344f08a4259228b201a
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-04-17 20:44:13 +0200

    Initialize project from Flinesoft/NewFrameworkTemplate on GitHub

---
+++ Sources/Supporting Files/LingoHubSync.h
@@ -0,0 +1,15 @@
+//
+//  Created by Cihat Gündüz on 27.06.17.
+//  Copyright © 2017 Jamit Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LingoHubSync.
+FOUNDATION_EXPORT double LingoHubSyncVersionNumber;
+
+//! Project version string for LingoHubSync.
+FOUNDATION_EXPORT const unsigned char LingoHubSyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LingoHubSync/PublicHeader.h>
+
---
+++ UsageExamples.playground/Contents.swift
@@ -0,0 +1,5 @@
+// swiftlint:disable file_header
+import Foundation
+import LingoHubSync
+
+// Add your usage examples here.
